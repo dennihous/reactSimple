b62bde064d34ffabc4cf6d47edcb656a249fc624
dennis <denhouston1@icloud.com>
2022-11-15 23:48:36 +0000

    Console returns letter clicked

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 function App() {
   const word = "Hello"
 
+  const handleClick = (char) => {
+    console.log("Clicked the", char)
+  }
+
   return (
     <div className="App">
       <h1>Main</h1>
       <div>
-        {word.split("").map(char => <button>{char}</button>)}
+        {word.split("").map(char => (
+        <button onClick={() => handleClick(char)}>{char}</button>
+        ))}
       </div>
     </div>
   );
