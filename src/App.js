79a79dfd32149737b5a84e39e45a2b29bb7a97d7
dennis <denhouston1@icloud.com>
2022-11-15 23:40:19 +0000

    Split each letter into a button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
 function App() {
-  const word = "Hello World"
+  const word = "Hello"
 
   return (
     <div className="App">
-      {word}
+      <h1>Main</h1>
+      <div>
+        {word.split("").map(char => <button>{char}</button>)}
+      </div>
     </div>
   );
 }
