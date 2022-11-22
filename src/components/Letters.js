c8f4284fecc249ae7f7dcf8b66740ecaacf88c17
dennis <denhouston1@icloud.com>
2022-11-22 11:53:30 +0000

    Refactoring

--- src/components/Letters.js
+++
@@ -1,8 +0,0 @@
-import { ReturnLetter } from "./ReturnLetter"
-
-export const Letters = ({setElement, word}) => {
-  return <div>{word.split("").map(char => {
-
-   return <ReturnLetter letter={char} handleClick={setElement} />
-  })}</div>
-}
--- src/components/ReturnLetter.js
+++
@@ -1,5 +0,0 @@
-export const ReturnLetter = ({handleClick, letter}) => {
-  return <button onClick={() => {
-    handleClick(letter)
-  }}>{letter}</button>
-}
