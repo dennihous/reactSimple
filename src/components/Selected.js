85ae9c1d72583ee9459127a88b7b58c6a5ab3de3
dennis <denhouston1@icloud.com>
2022-11-22 12:19:50 +0000

    Changed onClick to only offclick if char is the same

--- src/components/Selected.js
+++ src/components/Selected.js
@@ -1,7 +1,7 @@
 export const Selected = ({element, setElement, char }) => {
   return <button onClick={() => {
     console.log(element)
-    if(element === ""){
+    if(element === "" || element !== char){
       setElement(char)
     } else {
       setElement("")
