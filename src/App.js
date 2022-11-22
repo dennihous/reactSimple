8e111369686e6113628e6eb902bee0d398f85f49
dennis <denhouston1@icloud.com>
2022-11-22 12:15:56 +0000

    Refactored and added spacing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import './App.css';
 import { Element } from "./components/Element";
-// import { ReturnLetter } from './components/ReturnLetter';
 import { OnClick } from './components/OnClick';
-// import { Letters } from "./components/Letters";
 import React, { useState } from 'react';
-
-// function App() {
-//   const word = "Dennis"
-//   const [element, setElement] = useState("");
-
-//   return (
-//     <div className="App">
-//       <br />
-//       <br />
-//       <br />
-//       <Letters setElement={setElement} word={word} element={element} />
-      
-//     </div>
-//   );
-// }
 
 function App() {
   const word = "Dennis"
@@ -26,8 +9,10 @@ function App() {
 
   return (
     <div className="App">
+      <br />
+      <br />
+      <br />
       <OnClick word={word} setElement={setElement} element={element} />
-      {/* <Letters setElement={setElement} word={word} element={element} /> */}
       <Element element={element} />
     </div>
   );
--- src/components/Selected.js
+++ src/components/Selected.js
@@ -1,5 +1,6 @@
 export const Selected = ({element, setElement, char }) => {
   return <button onClick={() => {
+    console.log(element)
     if(element === ""){
       setElement(char)
     } else {
